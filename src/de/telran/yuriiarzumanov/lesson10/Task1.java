e3fd3ea194b3584a4d348c2a8495983c36bf8c45
Yurii <yura_arzumanov@icloud.com>
2022-12-17 18:21:45 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/lesson10/Task1.java
@@ -0,0 +1,24 @@
+package de.telran.yuriiarzumanov.lesson10;
+
+public class Task1 {
+    public static void main(String[] args) {
+        //В кассу на пятипроцентный вклад положили 200 рублей.
+        // Какой станет сумма вклада через 10 лет?
+        //5 процентов = 0.05 к примеру 3% от ста рублей 100 *0.03 = 3
+        // 1. нет капитализации -> сумма процентов не прибавляется к вкладу
+        // 2. с капитализацией -> сумма процентов прибавляется к вкладу в конце года
+        double income = 200.0;
+        int howManyYears = 10;
+        double percent = 0.05;
+        double finalSumWithoutCap = income;
+        double finalSumWithCap = income;
+        ///
+        for (int i = 0; i < howManyYears; i++) {// 200 210
+            finalSumWithoutCap += income * percent;
+            finalSumWithCap += finalSumWithCap * percent;
+        }
+        System.out.println(finalSumWithoutCap);
+        System.out.println(finalSumWithCap);
+
+    }
+}
