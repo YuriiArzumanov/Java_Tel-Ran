1274f8e7a628e5050ce0ec15b3e19f90e9292469
Yurii <yura_arzumanov@icloud.com>
2022-12-17 17:11:19 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/lesson10/ForTriangle.java
@@ -0,0 +1,18 @@
+package de.telran.yuriiarzumanov.lesson10;
+
+public class ForTriangle {
+    public static void main(String[] args) {
+        int howManyStars = 1;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < howManyStars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            howManyStars +=2;
+        }
+    }
+}
+
