91fb0139483a3c3d49423f7100319a1639deaea0
Yurii <yura_arzumanov@icloud.com>
2022-12-17 15:41:04 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/lesson10/ForExample.java
@@ -0,0 +1,14 @@
+package de.telran.yuriiarzumanov.lesson10;
+
+public class ForExample {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += i;
+            System.out.println(i);
+        }
+            System.out.println("Сумма" + sum);{
+            }
+
+    }
+}
