13446f66139d14de1e8b204db419357615bd39b4
Yurii <yura_arzumanov@icloud.com>
2022-12-17 17:09:02 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/lesson10/ForStella.java
@@ -0,0 +1,13 @@
+package de.telran.yuriiarzumanov.lesson10;
+
+public class ForStella {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            for ( int j = 0; j < 10; j++){
+                System.out.print( " * ");
+            }
+            System.out.println();
+        }
+    }
+
+}
