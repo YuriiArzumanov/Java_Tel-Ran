bffa24739080df22d7d09fd545158d5232a5f011
Yurii <yura_arzumanov@icloud.com>
2022-12-18 14:29:35 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/hw181222/Multiples.java
@@ -0,0 +1,16 @@
+package de.telran.yuriiarzumanov.hw181222;
+
+//Вывести на консоль произведение двузначных нечетных чисел кратных 13.
+
+
+public class Multiples {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            if (i % 13 == 0 && i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
