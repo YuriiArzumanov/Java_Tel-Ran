8869d5ef1730dc2cc8a87691be04dd6bbe67040d
Yurii <yura_arzumanov@icloud.com>
2022-12-18 13:56:13 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/hw181222/NumbersLess0.java
@@ -0,0 +1,13 @@
+package de.telran.yuriiarzumanov.hw181222;
+
+//Вывести на консоль числа от -10 до -40.
+
+public class NumbersLess0 {
+    public static void main(String[] args) {
+        int num = -10;
+        int num2 = -40;
+        for (int i = num2; i <=num ; i++) {
+            System.out.println(i);
+        }
+    }
+}
