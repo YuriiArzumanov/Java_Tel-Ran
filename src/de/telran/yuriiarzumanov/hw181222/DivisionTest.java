d52f3d52e77f1c86083e7ed7d10909d2b6b184ca
Yurii <yura_arzumanov@icloud.com>
2022-12-18 13:06:03 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/hw181222/DivisionTest.java
@@ -0,0 +1,15 @@
+package de.telran.yuriiarzumanov.hw181222;
+
+//Вывести на консоль те двузначные числа которые делятся на 4, но не делятся на 6.
+
+public class DivisionTest {
+    public static void main(String[] args) {
+        int sum = 0 ;
+        for (int i = 0; i <100 ; i++) {
+            if (i % 4 == 0 && i % 6 != 0 ) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
