e531a14453750dd4b3410bde486876330a42e53d
Yurii <yura_arzumanov@icloud.com>
2022-12-25 19:54:05 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/hw221222/Task1level1.java
@@ -0,0 +1,11 @@
+package de.telran.yuriiarzumanov.hw221222;
+
+// Есть два массива char[] first = {‘a’,’b’,’c’,’d’,’e’}
+// и char[] second = {‘f’,’g’,’h’,’i’,’g’}
+// создать массив соединяющий два этих массива в один новый
+
+public class Task1level1 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/de/telran/yuriiarzumanov/hw221222/Task4.java
+++ src/de/telran/yuriiarzumanov/hw221222/Task4.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args){
 
+
     }
 
 
