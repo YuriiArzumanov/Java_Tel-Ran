8b18f8a4109bdbfd4f6bc07289eef98bb78ec5b6
Yurii <yura_arzumanov@icloud.com>
2022-12-25 14:55:17 +0100

    init

---
+++ src/de/telran/yuriiarzumanov/lesson11/debugTest.java
@@ -0,0 +1,12 @@
+package de.telran.yuriiarzumanov.lesson11;
+
+public class debugTest {
+        public static void main(String[] args) {
+            for (int i = 0; i < 100; i++) {
+                if (i % 2 == 0 || i % 3 == 0) {
+                    System.out.println("labadada");
+                }
+            }
+        }
+    }
+
