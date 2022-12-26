2e22cb6d5723a7a1797ece06b0759b987e878d12
Yurii <yura_arzumanov@icloud.com>
2022-12-26 15:42:39 +0100

    init

--- src/de/telran/yuriiarzumanov/hw221222/Task1level1.java
+++ src/de/telran/yuriiarzumanov/hw221222/Task1level1.java
@@ -8,12 +8,17 @@ public class Task1level1 {
     public static void main(String[] args) {
         char[] first = {'a', 'b', 'c', 'd', 'e'};
         char[] second = {'f', 'g', 'h', 'i', 'g'};
+        char[] result = new char[first.length + second.length];
         for (int i = 0; i < first.length; i++) {
-            System.out.println(first[i]);
+            result[i] = first[i];
         }
-        for (int j = 0; j < second.length; j++) {
-            System.out.println(second[j]);
+        for (int i = 0; i < second.length; i++) {
+            result[i + first.length] = second[i];
         }
+        System.out.println(result);
 
     }
+
+
 }
+
