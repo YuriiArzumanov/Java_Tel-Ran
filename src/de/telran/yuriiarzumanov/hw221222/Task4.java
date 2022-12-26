b5b3eaf3d41ad4ca5e7b448b9f58afda6371f8ba
Yurii <yura_arzumanov@icloud.com>
2022-12-26 16:47:02 +0100

    init

--- src/de/telran/yuriiarzumanov/hw221222/Task4.java
+++ src/de/telran/yuriiarzumanov/hw221222/Task4.java
@@ -10,12 +10,7 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int number = scanner.nextInt();
 
     }
-
-
-
-
 }
